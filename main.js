96c74c4397159b58caf8fd03a0e4412e45b9b062
nilodude <alphone@protonmail.com>
2024-04-04 01:17:18 +0200

    panda jake

--- main.js
+++ main.js
@@ -1,28 +1,44 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-// const controls = new OrbitControls( camera, renderer.domElement );
-const loader = new GLTFLoader();
+const controls = new OrbitControls( camera, renderer.domElement );
 
+let scaleMouseY = 500
 let mausY = 0
-window.addEventListener('mousedown', (event)=>{
+window.addEventListener('mousemove', (event)=>{
     mausY = event.clientY
-    console.log(mausY)
+    console.log(mausY/scaleMouseY)
+})
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+scene.add( directionalLight );
+
+const objLoader = new OBJLoader();
+const textureLoader = new THREE.TextureLoader()
+
+objLoader.load('models/model001/1.obj', (objeto) => {
+   textureLoader.load('models/model001/1.png',(texture)=>{
+        const material = new THREE.MeshBasicMaterial( { map:texture } );
+        objeto.material = material
+        objeto.position.x = 0;
+        objeto.position.y = 0;
+        objeto.position.z = -10;
+        scene.add(objeto);
+    });     
 })
 
 let cubes = []
-let num = 10
+let num = 100
 for(let i = 0; i<= num;i++){
     const geometry = new THREE.BoxGeometry( 1, 1, 1 );
     const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
     const cube = new THREE.Mesh( geometry, material )
-    cube.position.x = 2*i - cubes.length -5
+    cube.position.x = 2*i - cubes.length -num/2
     cubes.push(cube)
     scene.add( cube );
 }
@@ -34,7 +50,7 @@ function animate() {
 	
     cubes.forEach(cube=>{
         cube.rotation.x += 0.01;
-        cube.rotation.y += mausY/500;
+        cube.rotation.y += mausY/scaleMouseY;
 
         cube.position.y = Math.sin(cube.rotation.y + cube.position.x)
     })
