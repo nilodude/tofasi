0b7229f0b91a8d0339eaa69496b105b751e94dbe
nilodude <alphone@protonmail.com>
2024-04-04 00:01:22 +0200

    amaxin

--- main.js
+++ main.js
@@ -4,30 +4,40 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+// const controls = new OrbitControls( camera, renderer.domElement );
+const loader = new GLTFLoader();
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+let mausY = 0
+window.addEventListener('mousedown', (event)=>{
+    mausY = event.clientY
+    console.log(mausY)
+})
+
+let cubes = []
+let num = 10
+for(let i = 0; i<= num;i++){
+    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const cube = new THREE.Mesh( geometry, material )
+    cube.position.x = 2*i - cubes.length -5
+    cubes.push(cube)
+    scene.add( cube );
+}
 
 camera.position.z = 5;
 
 
-
-const renderer = new THREE.WebGLRenderer();
-
-const controls = new OrbitControls( camera, renderer.domElement );
-const loader = new GLTFLoader();
-
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
 function animate() {
 	
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    cubes.forEach(cube=>{
+        cube.rotation.x += 0.01;
+        cube.rotation.y += mausY/500;
 
-
+        cube.position.y = Math.sin(cube.rotation.y + cube.position.x)
+    })
     
     requestAnimationFrame( animate );
 	renderer.render( scene, camera );
